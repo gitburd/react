52ca3f74616a6db412a44305f2b9cc2843d3dc1a
TPL6 <denaburd@gmail.com>
2019-03-26 15:50:40 -0700

    Updates

---
+++ precache-manifest.ab8024bf4048c2ec05d446f677ae0c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22f312793a848b6d1d54",
+    "url": "/react/static/css/main.c4bc110a.chunk.css"
+  },
+  {
+    "revision": "22f312793a848b6d1d54",
+    "url": "/react/static/js/main.109e93cc.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "a6d6fbb396ca8e978fdf",
+    "url": "/react/static/js/2.984f21df.chunk.js"
+  },
+  {
+    "revision": "77c1c041bb73a6deb5533471388985a1",
+    "url": "/react/index.html"
+  }
+];
